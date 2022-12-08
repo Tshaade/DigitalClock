e3c916e6d7c14eb7376be8d83c11b06379cd3c63
Tshaade <stahreem2000@gmail.com>
2022-12-08 17:57:25 -0500

    updated version

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+const hourEl = document.getElementById("hour");
+const minEl = document.getElementById("minutes");
+const secEl = document.getElementById("seconds");
+const amPmEl = document.getElementById("amPm");
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ap = "AM";
+
+    if(h > 12){
+        h = h - 12;
+        ap = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minEl.innerText = m;
+    secEl.innerText = s;
+    amPmEl, (innerText = ap);
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+}
+
+updateClock();
